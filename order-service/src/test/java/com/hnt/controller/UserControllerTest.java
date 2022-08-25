f6b031c1a7cba8711ceafa20c1f7c845fe7e8ad7
unknown <cogjava3160@iiht.tech>
2022-08-25 17:59:15 +0530

    day4

---
+++ order-service/src/test/java/com/hnt/controller/UserControllerTest.java
@@ -0,0 +1,14 @@
+package com.hnt.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UserControllerTest {
+
+	@Test
+	void testSaveUser1() {
+		fail("Not yet implemented");
+	}
+
+}
